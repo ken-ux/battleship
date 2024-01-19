25cd198f747e2d0e91d3c9bc3f1811dd47c32118
Kenny Nguyen <sokenns@gmail.com>
2024-01-18 16:04:32 -0800

    Add plans for Player tests

--- src/tests/Player.test.js
+++ src/tests/Player.test.js
@@ -1,1 +1,8 @@
-test("")
+test("Player and computer have their own gameboards");
+test("Player hits are recorded on computer's ships");
+test("Computer hits are recorded on player's ships");
+test("Computer can check if space has already been hit");
+describe("Computer will make a move after player did", () => {
+  test("Computer hits player's board");
+  test("Computer will not make a move if all ships are sunk for either player");
+});
