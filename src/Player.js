4e8fa4e07b086da1f204ac6bc45fc472dc4b4c63
Kenny Nguyen <sokenns@gmail.com>
2024-01-18 23:52:25 -0800

    Add test to confirm gameboards are defined

--- src/Player.js
+++ src/Player.js
@@ -1,3 +1,18 @@
-export default class Player {
-  constructor() {}
+import Gameboard from "./Gameboard";
+
+export class Player {
+  constructor() {
+    this.gameboard = new Gameboard();
+  }
+
+  makeMove() {}
+}
+
+export class Computer extends Player {
+  constructor() {
+    super();
+  }
+
+  // Modifies class function to hit random space
+  makeMove() {}
 }
--- src/tests/Player.test.js
+++ src/tests/Player.test.js
@@ -1,8 +1,21 @@
-test("Player and computer have their own gameboards");
-test("Player hits are recorded on computer's ships");
-test("Computer hits are recorded on player's ships");
-test("Computer can check if space has already been hit");
-describe("Computer will make a move after player did", () => {
-  test("Computer hits player's board");
-  test("Computer will not make a move if all ships are sunk for either player");
+import { Player, Computer } from "../Player";
+
+let player;
+let computer;
+beforeEach(() => {
+  player = new Player();
+  computer = new Computer();
 });
+
+test.only("Player and computer have their own gameboards", () => {
+  expect(player.gameboard).not.toBeUndefined();
+  expect(computer.gameboard).not.toBeUndefined();
+});
+
+// test("Player hits are recorded on computer's ships");
+// test("Computer hits are recorded on player's ships");
+// test("Computer can check if space has already been hit");
+// describe("Computer will make a move after player did", () => {
+//   test("Computer hits player's board");
+//   test("Computer will not make a move if all ships are sunk for either player");
+// });
