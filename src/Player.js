c44946b4650526447a5175cdf211370c345dc80c
Kenny Nguyen <sokenns@gmail.com>
2024-01-19 01:42:57 -0800

    Add tests and class methods for attacking players

--- src/Player.js
+++ src/Player.js
@@ -5,7 +5,11 @@ export class Player {
     this.gameboard = new Gameboard();
   }
 
-  makeMove() {}
+  // @param opponent is an instance of the Computer class
+  // @param position is an array containing the coordinates to attack, [x, y]
+  makeMove(opponent, position) {
+    opponent.gameboard.receiveAttack(position[0], position[1]);
+  }
 }
 
 export class Computer extends Player {
@@ -14,5 +18,18 @@ export class Computer extends Player {
   }
 
   // Modifies class function to hit random space
-  makeMove() {}
+  makeMove(opponent) {
+    const max = 10;
+    let randomX = Math.floor(Math.random() * max);
+    let randomY = Math.floor(Math.random() * max);
+
+    // Check if spaces are empty before attacking
+    while (opponent.gameboard.grid[randomX][randomY].isHit) {
+      randomX = Math.floor(Math.random() * max);
+      randomY = Math.floor(Math.random() * max);
+    }
+
+    opponent.gameboard.receiveAttack(randomX, randomY);
+    return [randomX, randomY];
+  }
 }
--- src/tests/Player.test.js
+++ src/tests/Player.test.js
@@ -7,13 +7,20 @@ beforeEach(() => {
   computer = new Computer();
 });
 
-test.only("Player and computer have their own gameboards", () => {
+test("Player and computer have their own gameboards", () => {
   expect(player.gameboard).not.toBeUndefined();
   expect(computer.gameboard).not.toBeUndefined();
 });
 
-// test("Player hits are recorded on computer's ships");
-// test("Computer hits are recorded on player's ships");
+test("Player hits are recorded on computer's ships", () => {
+  player.makeMove(computer, [0, 1]);
+  expect(computer.gameboard.grid[0][1].isHit).toBe(true);
+});
+
+test("Computer hits are recorded on player's ships", () => {
+  const position = computer.makeMove(player);
+  expect(player.gameboard.grid[position[0]][position[1]].isHit).toBe(true);
+});
 // test("Computer can check if space has already been hit");
 // describe("Computer will make a move after player did", () => {
 //   test("Computer hits player's board");
