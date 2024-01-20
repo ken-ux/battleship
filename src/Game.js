555998bcedb560eba7fa1726afea3266c8cf1dd9
Kenny Nguyen <sokenns@gmail.com>
2024-01-19 16:59:28 -0800

    Display gameboards

--- src/Game.js
+++ src/Game.js
@@ -1,2 +1,26 @@
 import "./style.css";
 import { Player, Computer } from "./Player.js";
+
+// Initialize player and computer
+const player = new Player();
+const computer = new Computer();
+
+function displaySquares(container, user) {
+  for (let i = 0; i < user.gameboard.grid.length; i++) {
+    const row = document.createElement("div");
+    row.classList = "row";
+    for (let j = 0; j < user.gameboard.grid[i].length; j++) {
+      const column = document.createElement("div");
+      column.textContent = `${i}, ${j}`;
+      row.appendChild(column);
+    }
+    container.appendChild(row);
+  }
+}
+
+// Display the player gameboard
+const playerGameboard = document.querySelector("#player-gameboard");
+displaySquares(playerGameboard, player);
+
+const computerGameboard = document.querySelector("#computer-gameboard");
+displaySquares(computerGameboard, computer);
